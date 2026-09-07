54b5c5fb6d98580bc478857680a9e08c16fd586c
Vishnu <vishnu@kuchnaya.com>
2026-09-07 16:39:59 +0530

    Revert "fix: externalize node packages to prevent edge runtime __dirname error"
    This reverts commit 7143bd911b02e07f16f79fc77406acbce805e4f5.

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['postgres', 'pg', 'cheerio', 'nodemailer'],
+  /* config options here */
 };
 
 export default nextConfig;
